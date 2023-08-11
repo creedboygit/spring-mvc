34cbadb4bf2743d26ed7669eb7c85c6b4ca6aa9f
creedboy <rich@hanssem.com>
2023-08-11 16:59:51 +0900

    jdbc 테스트

---
+++ src/test/java/org/example/UserDaoTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.SQLException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+public class UserDaoTest {
+
+    @BeforeEach
+    void setUp() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("db_schema.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+
+    @Test
+    void createTest() throws SQLException {
+
+        UserDao userDao = new UserDao();
+
+        userDao.create(new User("creed", "password", "name", "email"));
+
+        User user = userDao.findByUserId("creed");
+
+        assertThat(user).isEqualTo(new User("creed", "password", "name", "email"));
+    }
+}
